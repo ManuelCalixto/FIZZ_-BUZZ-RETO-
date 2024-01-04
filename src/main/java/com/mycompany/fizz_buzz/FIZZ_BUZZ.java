674e20e037b9d209cdb1e88a42db31383e8ed21f
ManuelCalixto <148130758+ManuelCalixto@users.noreply.github.com>
2024-01-04 03:00:13 -0600

    Update FIZZ_BUZZ.java

--- src/main/java/com/mycompany/fizz_buzz/FIZZ_BUZZ.java
+++ src/main/java/com/mycompany/fizz_buzz/FIZZ_BUZZ.java
@@ -5,21 +5,14 @@
  * - Múltiplos de 5 por la palabra "buzz".
  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz". */
 package com.mycompany.fizz_buzz;
-
 public class FIZZ_BUZZ {
+    
     public static void main(String[] args) {
-
         for (int i = 0; i < 101; i++) {
-            
-            //CONDICION PARA MULTIPLO DE 3
             if (i % 3 == 0) {
                 System.out.println("fizz");
-
-                //CONDICION PARA MULTIPLO DE 3
             } else if (i % 5 == 0) {
                 System.out.println("buzz");
-
-                //CONDICION PARA AMBOS
             } else if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("fizzbuzz");
             }else{
